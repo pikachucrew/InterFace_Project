cb5882aaa2964c48bb14ea72865e0a90dca0a48f
Jordan-Shaw <jordan.shaw2400@gmail.com>
2020-01-31 11:21:55 +0000

    add webcam face detection functi0nality on a camPage

--- interface/src/components/CamPage.jsx
+++ interface/src/components/CamPage.jsx
@@ -1,14 +1,67 @@
 import React, { Component } from 'react'
+import * as faceapi from 'face-api.js'
+import {Link} from '@reach/router'
 
 export default class CamPage extends Component {
-componentDidMount() {
-  
-}
+  componentDidMount() {
+    // import * as api from 'api'
+    const video = document.querySelector("#videoElement");
+    const canvas = document.querySelector('#canvasElement')
+
+    console.log('in script.js')
+
+    const startVideo = () => {
+      if (navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({ video: true })
+          .then(function (stream) {
+            video.srcObject = stream;
+          })
+          .catch(function (error) {
+            console.log("Something went wrong!");
+          });
+      }
+    }
+
+    Promise.all([
+      faceapi.nets.tinyFaceDetector.loadFromUri('/weights'),
+      faceapi.nets.faceRecognitionNet.loadFromUri('/weights'),
+      faceapi.nets.faceLandmark68Net.loadFromUri('/weights'),
+      faceapi.nets.faceExpressionNet.loadFromUri('/weights')
+    ]).then(startVideo())
+
+    video.addEventListener('play', () => {
+      //const canvas = faceapi.createCanvasFromMedia(video)
+      //document.body.append(canvas)
+      const displaySize = { width: video.width, height: video.height }
+      faceapi.matchDimensions(canvas, displaySize)
+      setInterval(async () => {
+        const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+
+        // console.log(detections)
+        const resizedDetections = faceapi.resizeResults(detections, displaySize)
+        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+        faceapi.draw.drawDetections(canvas, resizedDetections)
+        faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+        faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+      }, 100)
+
+    })
+  }
 
   render() {
     return (
       <div>
         <h2>View Camera</h2>
+        <div id="container">
+          <video autoPlay={true} id="videoElement" width="640" height="480">
+          </video>
+          <canvas id="canvasElement"></canvas>
+        </div>
+        <Link to="/dashboard">
+          <button>
+            Return
+          </button>
+        </Link>
       </div>
     )
   }
