0e8dbda5183491ccc28756f933ab8983a8b36573
mckeca <callumrmckenna@gmail.com>
2020-02-03 11:40:55 +0000

    changed react scripts for wsl

--- interface/src/App.js
+++ interface/src/App.js
@@ -1,24 +1,43 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import './App.css';
-import Header from './components/Header'
+import fire from './config';
+import Header from './components/Header';
 import Dashboard from './components/Dashboard';
 import CamPage from './components/CamPage';
-import { Router } from '@reach/router'
+import { Router } from '@reach/router';
 import LoginPage from './components/LoginPage';
-import {startVideo} from './utils/utils'
+import { startVideo } from './utils/utils';
 
 export default class App extends Component {
-  state = {}
+  state = {
+    user: {}
+  };
+
+  componentDidMount() {
+    this.authListener();
+  }
+
+  authListener = () => {
+    fire.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({ user });
+      } else {
+        this.setState({ user: null });
+      }
+    });
+  };
+
   render() {
+    const { user } = this.state;
     return (
       <div className="App">
-        <Header className="Header"/>
+        <Header className="Header" />
         <Router className="Router">
-          <LoginPage path='/' />
-          <Dashboard path='/dashboard' />
-          <CamPage path='/webcam' startVideo={startVideo}/>
+          <LoginPage path="/" user={user} />
+          <Dashboard path="/dashboard" user={user} />
+          <CamPage path="/webcam" startVideo={startVideo} />
         </Router>
       </div>
-    )
+    );
   }
 }
