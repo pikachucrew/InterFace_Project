1558e1fd20bd34cef51d36ad93134ab885b2d835
IanJPeek <IanJPeek@gmail.com>
2020-02-03 16:20:09 +0000

    donut data manipulation tdd done

--- interface/src/spec/utils.spec.js
+++ interface/src/spec/utils.spec.js
@@ -3,8 +3,27 @@ const donutManipulator = require ("../utils/dataUtils")
 
 
 describe("donutManipulator", () => {
-
-  it('when passed an empty array it returns an empty array', () => {expect(donutManipulator([])).to.eql([])
+ const testArray = [
+   [10, 5, 2, 5, 5, 8, 5],
+   [0, 5, 0, 5, 25, 5, 5],
+   [5, 5, 3, 5, 5, 7, 19],
+   [5, 2, 5, 16, 0, 5, 5]
+ ];
+  it('when passed an empty array it returns an array of zeros', () => {
     
+    expect(donutManipulator([])).to.eql([0, 0, 0, 0, 0, 0, 0]);
+  
   });
+
+  it('returns a single array when passed an array of arrays', () => {
+donutManipulator(testArray).forEach(item => {
+  console.log(item)
+  expect(item).to.be.a("number")})
+  });
+
+it("averages each index from the input array in the returned array", () =>{
+expect(donutManipulator(testArray)).to.eql([5, 4.25, 2.5, 7.75, 8.75, 6.25, 8.5])
+})
+
+
 });
