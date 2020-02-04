6918c7fa4c29f668be29e9b9f79d968a49fa53a2
IanJPeek <IanJPeek@gmail.com>
2020-02-04 13:10:21 +0000

    manipulate line graph data and test

--- interface/src/spec/utils.spec.js
+++ interface/src/spec/utils.spec.js
@@ -33,6 +33,16 @@ describe("donutManipulator", () => {
 });
 
 describe.only('lineManipulator', () => {
+const testArray = [
+  [10, 5, 2, 5, 5, 8, 5],
+  [0, 5, 0, 5, 25, 5, 5],
+  [5, 5, 3, 5, 5, 7, 19],
+  [5, 2, 5, 16, 0, 5, 5],
+  [10, 5, 2, 5, 5, 8, 5],
+  [0, 5, 0, 5, 25, 5, 5],
+  [5, 5, 3, 5, 5, 7, 19]
+];
+
   it('takes an array and returns an object', () => {
     expect(lineManipulator([])).to.be.an('object')
   });
@@ -42,4 +52,29 @@ describe.only('lineManipulator', () => {
       'neutral', 'happy', 'sad', 'angry', 'fearful', 'disgusted', 'surprised'
     )
   });
+
+  it('returns an object with arrays populated with zeros when passed an empty array', () => {
+    expect(lineManipulator([])).to.eql({
+      neutral: [0, 0, 0, 0, 0, 0, 0],
+      happy: [0, 0, 0, 0, 0, 0, 0],
+      sad: [0, 0, 0, 0, 0, 0, 0],
+      angry: [0, 0, 0, 0, 0, 0, 0],
+      fearful: [0, 0, 0, 0, 0, 0, 0],
+      disgusted: [0, 0, 0, 0, 0, 0, 0],
+      surprised: [0, 0, 0, 0, 0, 0, 0]
+    });
+  });
+
+it("returns an object with each array's values corresponding to a particular index of nested input arrays", () => {
+  expect(lineManipulator(testArray)).to.eql({
+    neutral: [10, 0, 5, 5, 10, 0, 5],
+    happy: [5, 5, 5, 2, 5, 5, 5],
+    sad: [2, 0, 3, 5, 2, 0, 3],
+    angry: [5, 5, 5, 16, 5, 5, 5],
+    fearful: [5, 25, 5, 0, 5, 25, 5],
+    disgusted: [8, 5, 7, 5, 8, 5, 7],
+    surprised: [5, 5, 19, 5, 5, 5, 19]
+  });
+});
+
 });
--- interface/src/utils/dataUtils.js
+++ interface/src/utils/dataUtils.js
@@ -1,7 +1,8 @@
 const donutManipulator = arr => {
   if (arr.length === 0) return [0, 0, 0, 0, 0, 0, 0];
 
-  return arr.slice(0, -2)
+  return arr
+    .slice(0, -2)
     .reduce(
       (returnArray, innerArray) => {
         innerArray.forEach((num, i) => {
@@ -14,19 +15,58 @@ const donutManipulator = arr => {
     .map(num => num / arr.length);
 };
 
-
 const lineManipulator = arr => {
+  if (arr.length === 0) {
+    return {
+      neutral: [0, 0, 0, 0, 0, 0, 0],
+      happy: [0, 0, 0, 0, 0, 0, 0],
+      sad: [0, 0, 0, 0, 0, 0, 0],
+      angry: [0, 0, 0, 0, 0, 0, 0],
+      fearful: [0, 0, 0, 0, 0, 0, 0],
+      disgusted: [0, 0, 0, 0, 0, 0, 0],
+      surprised: [0, 0, 0, 0, 0, 0, 0]
+    };
+  }
 
-  // if arr.length=0
+  return arr.reduce(
+    (returnObj, innerArr) => {
+      innerArr.forEach((num, i) => {
+        if (i === 0) {
+          returnObj.neutral.push(num)
+        }
+        if (i === 1) {
+          returnObj.happy.push(num)
+        }
+        if (i === 2) {
+          returnObj.sad.push(num)
+        }
+        if (i === 3) {
+          returnObj.angry.push(num)
+        }
+        if (i === 4) {
+          returnObj.fearful.push(num)
+        }
+        if (i === 5) {
+          returnObj.disgusted.push(num)
+        }
+        if (i === 6) {
+          returnObj.surprised.push(num)
+        }
+      });
+      return returnObj;
+    },
+    {
+      neutral: [],
+      happy: [],
+      sad: [],
+      angry: [],
+      fearful: [],
+      disgusted: [],
+      surprised: []
+    }
+  );
+};
 
-  return { neutral: 0, 
-    happy: 0, 
-    sad: 0, 
-    angry: 0, 
-    fearful: 0, 
-    disgusted: 0, 
-    surprised: 0}
+module.exports = { donutManipulator, lineManipulator };
 
-}
-
-module.exports = {donutManipulator, lineManipulator};
+// .slice(0, -2)?
